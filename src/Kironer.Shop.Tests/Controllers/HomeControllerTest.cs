a75bc28d07a4c0e1767ee8046a5d952c2d10bb01
Kirk Chen <kirkchen@nine-yi.com>
2015-06-25 10:43:59 +0800

    create website and test

---
+++ src/Kironer.Shop.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kironer.Shop;
+using Kironer.Shop.Controllers;
+
+namespace Kironer.Shop.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 排列
+            HomeController controller = new HomeController();
+
+            // 作用
+            ViewResult result = controller.Index() as ViewResult;
+
+            // 判斷提示
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ src/Kironer.Shop/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Kironer.Shop.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
